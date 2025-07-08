cda0c3c384a0fd5325508c212cc786051407a96a
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-07-08 09:27:46 -0300

    feat: Add new HomeUiContract.kt events to handle HomeScreen actions

--- feature/home/src/main/java/com/octopus/edu/feature/home/HomeUiContract.kt
+++ feature/home/src/main/java/com/octopus/edu/feature/home/HomeUiContract.kt
@@ -1,37 +1,31 @@
 package com.octopus.edu.feature.home
 
+import androidx.compose.runtime.Stable
 import com.octopus.edu.core.domain.model.Entry
-import com.octopus.edu.core.domain.model.Habit
 import com.octopus.edu.core.domain.model.Recurrence
-import com.octopus.edu.core.domain.model.Task
-import com.octopus.edu.core.domain.model.mock
 import com.octopus.edu.core.ui.common.base.ViewEffect
 import com.octopus.edu.core.ui.common.base.ViewEvent
 import com.octopus.edu.core.ui.common.base.ViewState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
+import java.time.LocalDate
+import java.time.LocalTime
 
-internal fun mockEntryList(count: Int): List<Entry> =
-    (1..count).map {
-        if (it % 2 == 0) {
-            Task.mock(it.toString())
-        } else {
-            Habit.mock(it.toString())
-        }
-    }
-
-internal fun Habit.getRecurrenceAsText(): String? =
+internal fun getRecurrenceAsStringRes(recurrence: Recurrence): Int =
     when (recurrence) {
-        Recurrence.Custom -> null
-        Recurrence.Daily -> "Daily"
-        Recurrence.Weekly -> "Weekly"
-        null -> null
+        Recurrence.Custom -> R.string.custom
+        Recurrence.Daily -> R.string.daily
+        Recurrence.Weekly -> R.string.weekly
+        Recurrence.None -> R.string.none
     }
 
 internal object HomeUiContract {
+    @Stable
     data class UiState(
         val entries: ImmutableList<Entry> = persistentListOf(),
         val isLoading: Boolean = false,
+        val entryCreationState: EntryCreationState = EntryCreationState(),
     ) : ViewState
 
     sealed interface UiEffect : ViewEffect {
@@ -40,5 +34,73 @@ internal object HomeUiContract {
         ) : UiEffect
     }
 
-    sealed interface UiEvent : ViewEvent
+    sealed interface UiEvent : ViewEvent {
+        sealed interface Entry {
+            data object Add : UiEvent
+
+            data object Save : UiEvent
+        }
+
+        sealed interface AddEntry {
+            data object Cancel : UiEvent
+
+            data object ShowSettingsPicker : UiEvent
+
+            data object HideSettingsPicker : UiEvent
+
+            data object ShowTimePicker : UiEvent
+
+            data object HideTimePicker : UiEvent
+
+            data object ShowRecurrencePicker : UiEvent
+
+            data object HideRecurrencePicker : UiEvent
+        }
+
+        data class UpdateEntryTitle(
+            val title: String,
+        ) : UiEvent
+
+        data class UpdateEntryDescription(
+            val description: String,
+        ) : UiEvent
+
+        data class UpdateEntryDate(
+            val date: LocalDate,
+        ) : UiEvent
+
+        data class UpdateEntryTime(
+            val hour: Int,
+            val minute: Int,
+        ) : UiEvent
+
+        data class UpdateEntryRecurrence(
+            val recurrence: Recurrence
+        ) : UiEvent
+    }
+
+    @Stable
+    internal data class EntryCreationState(
+        val isEntryCreationModeEnabled: Boolean = false,
+        val isSetEntryDateModeEnabled: Boolean = false,
+        val isSetEntryTimeModeEnabled: Boolean = false,
+        val isSetEntryRecurrenceModeEnabled: Boolean = false,
+        val currentEntryTitle: String? = null,
+        val currentEntryDescription: String? = null,
+        val currentEntryDate: LocalDate = LocalDate.now(),
+        val currentEntryTime: LocalTime? = null,
+        val currentEntryReminder: String? = null,
+        val currentEntryRecurrence: Recurrence? = null,
+    ) {
+        companion object {
+            val recurrenceOptions
+                get() =
+                    listOf(
+                        Recurrence.None,
+                        Recurrence.Daily,
+                        Recurrence.Weekly,
+                        Recurrence.Custom,
+                    ).toImmutableList()
+        }
+    }
 }
