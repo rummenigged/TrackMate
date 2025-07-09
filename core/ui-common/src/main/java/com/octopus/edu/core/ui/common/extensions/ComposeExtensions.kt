2b393edc6f5ce8fb6c975350f4b084e0dfed323b
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-07-09 15:36:57 -0300

    refactor: Create ComposeExtensions.kt and EntryExtensions.kt

---
+++ core/ui-common/src/main/java/com/octopus/edu/core/ui/common/extensions/ComposeExtensions.kt
@@ -0,0 +1,21 @@
+package com.octopus.edu.core.ui.common.extensions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun rememberMaxTextWidthDp(vararg strings: String): Dp {
+    val textMeasurer = rememberTextMeasurer()
+    val density = LocalDensity.current
+    return remember(*strings) {
+        with(density) {
+            strings
+                .maxOf { label ->
+                    textMeasurer.measure(label).size.width
+                }.toDp()
+        }
+    }
+}
