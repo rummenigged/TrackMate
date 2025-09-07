35f44310f7aa684c1ccd924d7f33532d46367c8a
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-09-07 11:24:27 -0300

    refactor: Add new feature:signIn and data:data-auth modules

---
+++ feature/signIn/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.octopus.edu.feature.signin"
+    compileSdk = rootProject.extra["compileSdkVersion"].toString().toInt()
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdkVersion"].toString().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = rootProject.ext["sourceCompatibility"] as JavaVersion
+        targetCompatibility = rootProject.ext["targetCompatibility"] as JavaVersion
+    }
+
+    kotlin {
+        jvmToolchain(rootProject.ext["kotlinOptionsJVMTarget"].toString().toInt())
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    packaging {
+        resources {
+            excludes.addAll(
+                setOf(
+                    "META-INF/LICENSE.md",
+                    "META-INF/LICENSE-notice.md",
+                ),
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:domain"))
+    implementation(project(":core:design"))
+    implementation(project(":core:data:data-auth"))
+    implementation(project(":core:ui-common"))
+
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.lottie.compose)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.credentials)
+
+    testImplementation(project(":core:testing"))
+    androidTestImplementation(project(":core:testing"))
+}
