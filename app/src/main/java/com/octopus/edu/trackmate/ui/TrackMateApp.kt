849db00e8c184d48a2cb41d3e202c4171a3bfcca
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-09-08 14:26:11 -0300

    feat: Handle app content route by user sign in status

--- app/src/main/java/com/octopus/edu/trackmate/ui/TrackMateApp.kt
+++ app/src/main/java/com/octopus/edu/trackmate/ui/TrackMateApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.SheetState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,28 +15,78 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.octopus.edu.core.ui.common.actions.Route
+import com.octopus.edu.core.ui.common.actions.Screen.Analytics
+import com.octopus.edu.core.ui.common.actions.Screen.History
+import com.octopus.edu.core.ui.common.actions.Screen.Home
+import com.octopus.edu.core.ui.common.actions.Screen.MainContent
+import com.octopus.edu.core.ui.common.actions.Screen.SignIn
 import com.octopus.edu.core.ui.common.actions.TrackMateNavigationActions
 import com.octopus.edu.feature.analytics.AnalyticsScreen
 import com.octopus.edu.feature.history.HistoryScreen
 import com.octopus.edu.feature.home.HomeScreen
 import com.octopus.edu.feature.home.components.AddEntryBottomLayout
 import com.octopus.edu.feature.home.createEntry.AddEntryViewModel
+import com.octopus.edu.feature.signin.AuthUiContract.UiState
+import com.octopus.edu.feature.signin.AuthViewModel
+import com.octopus.edu.feature.signin.SignInScreen
 import com.octopus.edu.trackmate.navigation.TrackMateNavigationWrapper
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun TrackMateApp() {
+internal fun TrackMateApp(viewModel: AuthViewModel = hiltViewModel()) {
     val navController = rememberNavController()
-    val navActions =
-        remember(navController) {
-            TrackMateNavigationActions(navController)
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    val initialDestination = if (uiState is UiState.Authenticated) MainContent else SignIn
+
+    LaunchedEffect(uiState, navController) {
+        val currentRoute = navController.currentBackStackEntry?.destination?.route
+        if (uiState is UiState.Authenticated) {
+            if (currentRoute != MainContent.name) {
+                navController.navigate(MainContent) {
+                    popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }
+        } else if (currentRoute != SignIn.name) {
+            navController.navigate(SignIn) {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                launchSingleTop = true
+            }
         }
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    }
+
+    Surface {
+        NavHost(
+            navController = navController,
+            startDestination = initialDestination,
+        ) {
+            composable<SignIn> {
+                SignInScreen(viewModel = viewModel)
+            }
+
+            composable<MainContent> {
+                MainAppContent()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MainAppContent(addEntryViewModel: AddEntryViewModel = hiltViewModel()) {
+    val mainContentNavController = rememberNavController()
+    val navActions =
+        remember(mainContentNavController) {
+            TrackMateNavigationActions(mainContentNavController)
+        }
+    val navBackStackEntry by mainContentNavController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
     var showEntryCreationSheet by remember { mutableStateOf(false) }
@@ -43,8 +94,6 @@ internal fun TrackMateApp() {
         rememberModalBottomSheetState(
             skipPartiallyExpanded = true,
         )
-
-    val addEntryViewModel: AddEntryViewModel = hiltViewModel()
 
     val openEntryCreationSheet = remember { { showEntryCreationSheet = true } }
 
@@ -56,43 +105,41 @@ internal fun TrackMateApp() {
             }
         }
 
-    Surface {
-        TrackMateNavigationWrapper(
-            currentDestination = currentDestination,
-            navigateToTopLevelDestination = navActions::navigateTo,
+    TrackMateNavigationWrapper(
+        currentDestination = currentDestination,
+        navigateToTopLevelDestination = navActions::navigateTo,
+    ) {
+        NavHost(
+            navController = mainContentNavController,
+            startDestination = Home,
         ) {
-            NavHost(
-                navController = navController,
-                startDestination = Route.Home,
-            ) {
-                composable<Route.Home> {
-                    HomeScreen(
-                        modifier = Modifier.testTag("HomeScreen"),
-                        onFabClicked = openEntryCreationSheet,
-                    )
-                }
-
-                composable<Route.History> {
-                    HistoryScreen()
-                }
-
-                composable<Route.Analytics> {
-                    AnalyticsScreen()
-                }
-            }
-        }
-
-        if (showEntryCreationSheet) {
-            ModalBottomSheet(
-                containerColor = colorScheme.surface,
-                onDismissRequest = closeEntryCreationSheet,
-                sheetState = sheetState,
-            ) {
-                AddEntryBottomLayout(
-                    viewModel = addEntryViewModel,
-                    onFinished = closeEntryCreationSheet,
+            composable<Home> {
+                HomeScreen(
+                    modifier = Modifier.testTag("HomeScreen"),
+                    onFabClicked = openEntryCreationSheet,
                 )
             }
+
+            composable<History> {
+                HistoryScreen()
+            }
+
+            composable<Analytics> {
+                AnalyticsScreen()
+            }
+        }
+    }
+
+    if (showEntryCreationSheet) {
+        ModalBottomSheet(
+            containerColor = colorScheme.surface,
+            onDismissRequest = closeEntryCreationSheet,
+            sheetState = sheetState,
+        ) {
+            AddEntryBottomLayout(
+                viewModel = addEntryViewModel,
+                onFinished = closeEntryCreationSheet,
+            )
         }
     }
 }
