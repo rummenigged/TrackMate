9975a2fc0c335cd048500439683594e01ca44640
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-10-17 13:31:03 -0300

    refactor: Create the error classifiers concrete implementations

---
+++ core/data/data-entry/src/main/java/com/octopus/edu/core/data/entry/DatabaseErrorClassifier.kt
@@ -0,0 +1,19 @@
+package com.octopus.edu.core.data.entry
+
+import android.database.sqlite.SQLiteCantOpenDatabaseException
+import android.database.sqlite.SQLiteDiskIOException
+import android.database.sqlite.SQLiteFullException
+import com.octopus.edu.core.domain.utils.BaseErrorClassifier
+import java.sql.SQLTimeoutException
+
+class DatabaseErrorClassifier : BaseErrorClassifier() {
+    override fun isTransient(throwable: Throwable): Boolean =
+        when (throwable) {
+            is SQLiteDiskIOException,
+            is SQLiteFullException,
+            is SQLTimeoutException,
+            is NoSuchElementException,
+            is SQLiteCantOpenDatabaseException -> true
+            else -> false
+        }
+}
---
+++ core/data/data-entry/src/main/java/com/octopus/edu/core/data/entry/SyncErrorClassifier.kt
@@ -0,0 +1,18 @@
+package com.octopus.edu.core.data.entry
+
+import com.octopus.edu.core.data.entry.di.DatabaseErrorClassifierQualifier
+import com.octopus.edu.core.data.entry.di.NetworkErrorClassifierQualifier
+import com.octopus.edu.core.domain.model.common.ErrorType.TransientError
+import com.octopus.edu.core.domain.utils.BaseErrorClassifier
+import com.octopus.edu.core.domain.utils.ErrorClassifier
+
+class SyncErrorClassifier(
+    @field:DatabaseErrorClassifierQualifier
+    private val databaseErrorClassifier: ErrorClassifier,
+    @field:NetworkErrorClassifierQualifier
+    private val networkErrorClassifier: ErrorClassifier
+) : BaseErrorClassifier() {
+    override fun isTransient(throwable: Throwable): Boolean =
+        databaseErrorClassifier.classify(throwable) is TransientError ||
+            networkErrorClassifier.classify(throwable) is TransientError
+}
