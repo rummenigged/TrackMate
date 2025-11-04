8dc13f58e011b829c97392511ec5ecaad27ab55f
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-11-04 14:22:56 -0300

    ci: Update the annotation site targets to make it indicate the correct way the dependency injection should be done by Hilt

--- core/ui-common/src/main/java/com/octopus/edu/core/ui/common/StringResource.kt
+++ core/ui-common/src/main/java/com/octopus/edu/core/ui/common/StringResource.kt
@@ -4,6 +4,6 @@ import androidx.annotation.PluralsRes
 import androidx.annotation.StringRes
 
 data class StringResource(
-    @param:PluralsRes @param:StringRes val resId: Int,
+    @field:PluralsRes @field:StringRes val resId: Int,
     val formatArgs: List<Any> = emptyList()
 )
