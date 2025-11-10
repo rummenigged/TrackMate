89b21d965e7fd19a599d0bb5da5b20ca04b683cb
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-11-10 13:07:17 -0300

    refactor: Move Application Coroutine Scope provider from app to core:common module

--- app/src/main/java/com/octopus/edu/trackmate/sync/EntrySyncManager.kt
+++ app/src/main/java/com/octopus/edu/trackmate/sync/EntrySyncManager.kt
@@ -3,11 +3,11 @@ package com.octopus.edu.trackmate.sync
 import com.octopus.edu.core.common.DispatcherProvider
 import com.octopus.edu.core.common.Logger
 import com.octopus.edu.core.common.RetryPolicy
+import com.octopus.edu.core.common.di.ApplicationScope
 import com.octopus.edu.core.data.entry.di.SyncErrorClassifierQualifier
 import com.octopus.edu.core.domain.repository.EntryRepository
 import com.octopus.edu.core.domain.scheduler.EntrySyncScheduler
 import com.octopus.edu.core.domain.utils.ErrorClassifier
-import com.octopus.edu.trackmate.di.ApplicationScope
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.catch
